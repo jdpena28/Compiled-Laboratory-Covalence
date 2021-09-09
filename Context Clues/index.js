be858fe4bf86d9de941ffecf207bc2e2280e8dc5
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-09 10:13:11 +0800

    Create DOM h3 Element using JS

--- Context Clues/index.js
+++ Context Clues/index.js
@@ -1,10 +1,25 @@
-var h3Accusation = document.createElement('h3')
-h3Accusation.classList.add('accusation')
+var friendsName = ['Hudas','Eskaryote','Mico','Denmark','Susej']
+var locations = ['Judys','Basurahan','Marawi','New Guinea','New Tokyo','Manila',
+'Helsinki','Denver','Zamboanga','Nairobi']
+var weapons = ['Pencil','Ballpen','Knife','Ketchup','Chicken Wings','Paperclip','Fork','Mouse','Keyboard','Everson',
+'Laptop','Fan','Chalk','Notebook','flashdrivee','Graphics Card','RAM','Cotton Buds','Burger','Mouse Pad',
+'Fries','Oil','Dildo','Vibrator','Gel','Condom','Lubricant','Durex','Fleshlight','Hotdog']
 
 
-console.log(h3Accusation.className)
-
-for(var i = 0 ; i<=100; i++) {
-     h3Accusation.innerHTML = `Accusation ${i}`
-     document.body.appendChild(h3Accusation)
+for(var i = 1 ; i<=100; i++) {
+    createDOM(i)
 }
+
+function createDOM(accusationNum) {
+    var h3Accusation = document.createElement('h3')
+    h3Accusation.className = 'accusation'
+    h3Accusation.innerHTML = `Accusation ${accusationNum}`
+    document.body.appendChild(h3Accusation)
+    h3Accusation.addEventListener('click', () => {
+        alert('Accusation i: I accuse FRIEND_NAME, with the WEAPON_NAME in the LOCATION_NAME!')
+    })
+}
+
+
+
+
